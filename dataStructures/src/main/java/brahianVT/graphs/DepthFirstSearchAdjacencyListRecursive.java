9eda06bd4b24292b902901bdedc0b5eb5d8b68fd
BrahianVT <pumasemj@hotmail.com>
2020-07-09 22:33:55 -0600

    Depth First Search in a graph with a Adjacency List

---
+++ dataStructures/src/main/java/brahianVT/graphs/DepthFirstSearchAdjacencyListRecursive.java
@@ -0,0 +1,55 @@
+
+package brahianVT.graphs;
+
+/**
+	An implementation of a  weighted graph recursive approach to DFS 
+	in a 
+	TC: O(V + E)
+	@author Brahian VT
+*/
+
+import java.util.*;
+
+public class DepthFirstSearchAdjacencyListRecursive{
+		
+	static class Edge{
+		int from, to, cost;
+		
+		public Edge(int from, int to, int cost){
+			this.cost = cost;
+			this.to = to;
+			this.from = from;
+		}
+	}
+
+	public void addDirectedEdge(Map<Integer, List<Edge>> graph, int from, int to, int cost){
+		
+		List<Edge> list = graph.get(from);
+		
+		if(list == null){
+			list = new ArrayList<>();
+			graph.put(from, list);
+		}
+		
+		list.add(new Edge(from, to, cost));
+		
+	}
+	
+	public void dfs(int at, boolean[] visited, Map<Integer, List<Edge>> graph){
+		if(visited[at]) return 0L;
+		
+		visited[at] = true;
+		
+		
+		List<Edge> edges = graph.get(at);
+		
+
+		if(edges != null){
+			for(Edge edge: edges){
+				System.out.print(" " + edge.from);
+				dfs(edge.to, visited, graph);
+			}
+		}
+	}
+	
+}
