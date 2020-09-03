e0613cfbd1420758bc0f4d373f1e01bbcc8a87fc
BrahianVT <pumasemj@hotmail.com>
2020-09-02 21:31:19 -0600

    added TopologicalSortAdjacencyListTest

--- dataStructures/src/main/java/brahianVT/graphs/TopologicalSortAdjacencyList.java
+++ dataStructures/src/main/java/brahianVT/graphs/TopologicalSortAdjacencyList.java
@@ -4,13 +4,12 @@ package brahianVT.graphs;
 
 /**
 	Implementation of topological sort, taking an adjaceny list of an acyclic graph
-	https://www.youtube.com/watch?v=eL-KzMXSXXI&list=PLDV1Zeh2NRsDGO4--qE8yH72HFL1Km93P&index=15
 */
 import java.util.*;
 
 public class TopologicalSortAdjacencyList{
 	
-	static class Edge{
+	public static class Edge{
 		int from, to ,weight;
 		
 		public Edge(int f, int t, int w) {
@@ -24,17 +23,19 @@ public class TopologicalSortAdjacencyList{
 	int i, int at, boolean[] visited, int[] ordering, Map<Integer, List<Edge>> graph){
 		visited[at] = true;
 		List<Edge> edges = graph.get(at);
+		
 		if(edges != null)
 			for(Edge edge: edges)
 				if(!visited[edge.to])
 					i =  dfs(i, edge.to, visited, ordering, graph);
+		
 		
 		ordering[i] = at;
 		
 		return i -1;
 	}
 	
-	public int[] topologicalSort(Map<Integer, List<Edge>> graph, int numNodes){
+	public static int[] topologicalSort(Map<Integer, List<Edge>> graph, int numNodes){
 		int[] ordering = new int[numNodes];
 		boolean[] visited = new boolean[numNodes];
 		int i = numNodes - 1;
@@ -46,5 +47,31 @@ public class TopologicalSortAdjacencyList{
 		return ordering;
 	}
 	
+	/* A simple application for topological sort is find the shortest path  between two nodes
+	   in a Directed Acyclic Graph, here finds the shortest path to all nodes starting at "start"
+	*/
+	public static Integer[] dagShortestPath(Map<Integer, List<Edge>> graph, int start, int numNodes){
+		
+		int[] topSort = topologicalSort(graph, numNodes);
+		Integer[] dist = new Integer[numNodes];
+		
+		dist[start] = 0;
+		
+		for(int i = 0; i < numNodes; i++){
+			int nodeIndex = topSort[i];
+			
+			if(dist[nodeIndex] != null){
+				List<Edge> adjacentEdges = graph.get(nodeIndex);
+				if( adjacentEdges != null){
+					for(Edge edge: adjacentEdges){
+						int newDist = dist[nodeIndex] + edge.weight;
+						if (dist[edge.to] == null){ dist[edge.to] = newDist; }
+						else dist[edge.to] = Math.min(newDist, dist[edge.to]); 
+					}
+				}
+			}
+		}
+		return dist;
+	}
 	
 }
